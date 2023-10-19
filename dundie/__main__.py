e6d3022792ef8069508002f01dd8f24aece66a0a
Charles Bambineti <bambineti@gmail.com>
2023-10-18 21:52:42 -0300

    terceiro

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,2 +1,41 @@
+import argparse
+
+def load(filepath):
+    """ Loads data from filepath to the database"""
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not Found {e}")
+
+
 def main():
-    print(" Execuntando Dundie")
+    parser = argparse.ArgumentParser(
+    description="Dundie CLI",
+    epilog="Enjoy and use with cautions.",
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="Ajuda",
+        choices=('load',"show","send"),
+        default="help",
+    )
+
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+        default=None,
+    )
+
+    args = parser.parse_args()
+    try:
+        globals()[args.subcommand](args.filepath)
+    except KeyError:
+        print("Subcommand is invalid")
+
+
+if __name__ == "__main__":
+    main()
