31137770979460bc3172ee12d5f5e2f389a91b61
Charles Bambineti <bambineti@gmail.com>
2023-10-18 21:00:04 -0300

    Meu primiero commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Dundie", 
+    version="0.1.0",
+    description="Teste 2 Teste",
+    author="Charles Bambineti",
+    packages=find_packages(),
+)
